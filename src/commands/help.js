0208b86252d95a8e2998bd9399ed6eebdd14b673
Skylar Lowery <skylar.lowery21@gmail.com>
2015-09-18 15:02:09 -0600

    Implement help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,21 @@
-var commands = require('../core/commands');
+var print = require('../core/print');
+var _ = require('underscore');
+var Table = require('cli-table');
 
 module.exports = function() {
-    // todo
+    // async include of commands (circular dependency)
+    var commands = require('../core/commands');
+    var table = new Table({
+        head: [ 'Command', 'Description' ],
+        colWidths: [ 30, 120 ]
+    });
+    _.each(commands, function(mod, command) {
+        table.push([command, mod.moduleDescription]);
+    });
+    print.info('Thanks for using JIRA Pal, available commands are:');
+    print.log(table.toString());
+    print.info('Use [jira help command-name] to get more details on certain commands\n');
+    print.info('\tExample: jira help login\n');
 };
 
 // unecessary but added to be explicit

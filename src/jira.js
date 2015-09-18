2e4d7edb58870a31cd0242b93bab0a96d5da1c7a
Skylar Lowery <skylar.lowery21@gmail.com>
2015-09-18 11:28:49 -0600

    If creds file is not found, create it
    * Prompts WIP

--- src/jira.js
+++ src/jira.js
@@ -15,3 +15,20 @@ _.each(fs.readdirSync(__dirname + '/commands'), function(filename) {
         print.fail(_s.sprintf('Could not load command %s because %s', name, e && e.message));
     }
 });
+
+/*
+ * Check to see if the credentials module exists, if not
+ * create it.
+ * */
+if (!fs.exists(settings.credentialsFileLocation)) {
+    print.info('Initial login, creating creds file');
+    commands.logout();
+}
+
+var creds = require(settings.credentialsFileLocation.replace(/\.js$/, ''));
+
+if (creds === null) {
+    print.info('You should log in');
+} else {
+    print.info('Hi there', creds);
+}

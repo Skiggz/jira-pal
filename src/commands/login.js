685b479274bd90c261fe63045d5cba02048a1f88
Skylar Lowery <skylar.lowery21@gmail.com>
2015-09-18 11:17:46 -0600

    Add rudimentary login/logout commands

---
+++ src/commands/login.js
@@ -0,0 +1,21 @@
+var _s = require('underscore.string');
+var fs = require('fs');
+var Buffer = require('buffer');
+/*
+* Username and Password base64 auth encoding
+* is stored in the credentials file and used
+* for each request. It's like a dumb file
+* store as a node module. It's not super
+* awesome or anything, but it's easy and
+* most people should be sharing that file,
+* so in majority of cases it's pretty secure.
+* I've added the file to git ignore to help
+* silly developers from accidentally pushing
+* credentials in a PR or fork as well.
+* */
+module.exports = function(username, password) {
+    // store basic base64 auth creds in creds module file
+    var login = new Buffer(_s.sprintf('%s:%s', username, password)).toString('base64');
+    fs.writeFileSync(__dirname + '/data/credentials.js', _s.sprintf('module.exports = \'%s\';'), login);
+
+};
---
+++ src/commands/logout.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+module.exports = function() {
+    /*
+    * The API for jira is oauth or basic auth, for simplicity
+    * (and avoiding implementing some narly terminal oauth)
+    * we store the base64 user/pass combo in a file locally
+    * This file is ignored by github so you don't accidentally
+    * push your password anywhere.
+    *
+    * In the future, it would be nice to do something with oauth
+    * but as a dev tool, it seems unnecessary. All communication
+    * with jira is over HTTPS, and hopefully your jira password
+    * isn't the same password you use for your bank. If you're
+    * worried, change it, or implement a friendly terminal oauth
+    * solution :)
+    * */
+    fs.writeFileSync(__dirname + '/data/credentials.js', 'module.exports = null;');
+};

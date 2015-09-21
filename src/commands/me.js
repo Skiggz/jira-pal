2cc29bd95a566b2e3e6105ee736a3fbdfaf8bde3
Skylar Lowery <skylar.lowery21@gmail.com>
2015-09-21 10:44:05 -0600

    List main story info for jira me

--- src/commands/me.js
+++ src/commands/me.js
@@ -11,10 +11,17 @@ module.exports = function() {
     api.search(
         api.queryBuilder().search.fields.assignee().equals(settings.username)
     ).then(function(response) {
+        var issues = [];
         _.each(JSON.parse(response.payload).issues, function(issueJson) {
-            print.log(
-                new JiraIssue(issueJson)
-            );
+            issues.push(new JiraIssue(issueJson));
+        });
+        var rows = [];
+        _.each(issues, function(issue) {
+            rows.push([issue.key, issue.type(), issue.summary()]);
+        });
+        print.table([ 'ID', 'Type', 'Summary' ], rows, {
+            thin: true,
+            colors: ['blue']
         });
     }, function(error) {
         print.log('Failed to make request', error);
--- src/models/issue.js
+++ src/models/issue.js
@@ -2,11 +2,26 @@ var _ = require('underscore');
 var JiraFields = require('./fields.js');
 module.exports = function JiraIssue(dataObject) {
 
+    var self = this;
     this.id = _.has(dataObject, 'id') ? dataObject.id : null;
     this.expand = _.has(dataObject, 'expand') ? dataObject.expand : null;
     // JIRA calls the ID linked url "self"
     this.url = _.has(dataObject, 'self') ? dataObject.self : null;
     this.key = _.has(dataObject, 'key') ? dataObject.key : null;
     this.fields = _.has(dataObject, 'fields') ? new JiraFields(dataObject.fields) : null;
+
+    this.type = function() {
+        if (self.fields && self.fields.issuetype && self.fields.issuetype.name) {
+            return self.fields.issuetype.name;
+        }
+        return 'Unknown';
+    };
+
+    this.summary = function() {
+        if (self.fields && self.fields.summary) {
+            return self.fields.summary;
+        }
+        return '';
+    };
 
 };

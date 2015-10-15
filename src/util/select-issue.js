3900b1e21c8e213a261859f7d5a31cdfd48c5694
Skylar Lowery <skylar.lowery21@gmail.com>
2015-10-15 15:18:46 -0600

    Add ability to select multiple issues via util

--- src/util/select-issue.js
+++ src/util/select-issue.js
@@ -4,22 +4,34 @@ var _ = require('underscore');
 var print = require('../core/print');
 var _s = require('underscore.string');
 
-module.exports = function(query) {
+module.exports = function(query, multipleChoice) {
     return new Promise(function(resolve, reject) {
         api.search(query).then(function(response) {
             var issues = response.data;
             if (issues.length === 0) {
                 return resolve(null);
             }
-            var q = print.question('list', 'story', 'Select a story');
+            var q = print.question(multipleChoice ? 'checkbox' : 'list', 'story', 'Select a story');
             _.each(issues, function(issue) {
-                q.choice(issue.key + ': ' + issue.summary());
+                if (multipleChoice) {
+                    q.checkbox(issue.key + ': ' + issue.summary());
+                } else {
+                    q.choice(issue.key + ': ' + issue.summary());
+                }
             });
             print.ask(q).then(function(answers) {
                 var answer = answers.story;
-                resolve(_.find(issues, function(it) {
-                    return _s.startsWith(answer, it.key);
-                }));
+                if (multipleChoice) {
+                    resolve(_.filter(issues, function(it) {
+                        return _.any(answer, function(keyAndSummary) {
+                            return _s.startsWith(keyAndSummary, it.key);
+                        });
+                    }));
+                } else {
+                    resolve(_.find(issues, function(it) {
+                        return _s.startsWith(answer, it.key);
+                    }));
+                }
             });
         }, function(e) {
             print.fail('Failed to fetch issues ' + e ? e.message : '');
